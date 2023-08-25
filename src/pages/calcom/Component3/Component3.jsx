95552ac28f64c72ea11c66a30ef7364470355672
Neeraj Vuppu <vneeraj1209@gmail.com>
2023-08-25 22:20:26 +0530

    11th commit

--- src/pages/calcom/Component3/Component3.jsx
+++ src/pages/calcom/Component3/Component3.jsx
@@ -8,9 +8,17 @@ const Component3 = () => {
         <p className=' calcom-Component3-heading'>How We Started?</p>
         <div className=' calcom-Component3-div2'>
           <p className='calcom-Component3-div2-text'>
-          After growing calcom Cements for over 15 years, we realized there was another dire shortage of cement in North-East India in the early 2000s. Infrastructural development was at an all-time high and cement prices were soaring because of the deficit in supply. Paradoxically the region still had a wealth of untapped raw materials. ​It was a rare opportunity to turn economic struggles into triumph and it sparked the inception of Calcom Cements​.          </p>
+          After growing Vinay Cements for over 15 years, we realized there was another dire shortage of cement in North-East India in the early 2000s. Infrastructural development was at an all-time high and cement prices were soaring because of the deficit in supply. 
+          </p>
           <p className='calcom-Component3-div2-text'>
-          The company is a manifestation of the founders ​visionary ​leadership, ​strategic ingenuity, and audacious risk-taking​. This was ​fortified by the​ir ​deep understanding of supply chain dynamics, a ​rich ​two-decade legacy in the cement industry, and over forty years of industrial acumen.​ ​​As a beacon of innovation ​Calcom ​reshaped industry benchmarks with an EBITDA that eclipses the industry average by 2.5 times. ​​​The company is a testament to the​ ​​founders' ​unyielding perseverance​ and hard work.          </p>
+          Paradoxically the region still had a wealth of untapped raw materials. ​It was a rare opportunity to turn economic struggles into triumph and it sparked the inception of Calcom Cements​.          
+          </p>
+          <p className='calcom-Component3-div2-text'>
+          The company is a manifestation of the founders ​visionary ​leadership, ​strategic ingenuity, and audacious risk-taking​. This was ​fortified by the​ir ​deep understanding of supply chain dynamics, a ​rich ​two-decade legacy in the cement industry, and over forty years of industrial acumen.         
+          </p>
+          <p className='calcom-Component3-div2-text'>
+          ​ ​​As a beacon of innovation ​Calcom ​reshaped industry benchmarks with an EBITDA that eclipses the industry average by 2.5 times. ​​​The company is a testament to the​ ​​founders' ​unyielding perseverance​ and hard work. 
+          </p>
         </div>
         <div className=' calcom-Component3-img-container'>
             <img className=' calcom-Component3-left' src={leftlogo}/>
--- src/pages/vinay/Component3/Component3.jsx
+++ src/pages/vinay/Component3/Component3.jsx
@@ -8,9 +8,14 @@ const Component3 = () => {
         <p className=' vinay-Component3-heading'>How We Started?</p>
         <div className=' vinay-Component3-div2'>
           <p className='vinay-Component3-div2-text'>
-          In the early 1980s we were in the process of building our house. Six months later, the builder told us he had to halt construction because there was a shortage of cement. We scoped the market but demand far outstripped supply and we had to stop construction for over three months.             </p>
+          In the early 1980s we were in the process of building our house. Six months later, the builder told us he had to halt construction because there was a shortage of cement. We scoped the market but demand far outstripped supply and we had to stop construction for over three months.             
+          </p>
           <p className='vinay-Component3-div2-text'>
-          People say one of my strengths is ‘root-cause analysis’ and going beyond the obvious. I spent those three months understanding the reasons for the asymmetry between demand and supply; learning how cement is manufactured; and understanding the technology, processes, and logistics. I discovered Assam was a prime spot to manufacture cement. After six months of research, brainstorming, and due diligence, opportunity beckoned. The rest is history, Vinay Cements was born the following year.          </p>
+          People say one of my strengths is ‘root-cause analysis’ and going beyond the obvious. I spent those three months understanding the reasons for the asymmetry between demand and supply; learning how cement is manufactured; and understanding the technology, processes, and logistics. 
+          </p>
+          <p className='vinay-Component3-div2-text'>
+            I discovered Assam was a prime spot to manufacture cement. After six months of research, brainstorming, and due diligence, opportunity beckoned. The rest is history, Vinay Cements was born the following year.          
+          </p>
         </div>
         <div className=' vinay-Component3-img-container'>
             <img className=' vinay-Component3-left' src={leftlogo}/>
