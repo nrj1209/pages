87c9629248e0bb3a063b1a57c3d8d66f7b1faf28
Neeraj Vuppu <vneeraj1209@gmail.com>
2023-09-18 22:09:31 +0530

    28thcommit

--- src/pages/road/Component1/Component1.jsx
+++ src/pages/road/Component1/Component1.jsx
@@ -3,7 +3,7 @@ import '../Component1/Component1.css'
 
 const Component1 = () => {
   return (
-    <div className='road-Component1-div'>
+    <div className='road-Component1-div child'>
         <p className='road-Component1-text1'>For-Profit</p>
         <p className='road-Component1-text2'>Serving India since</p>
         <p className='road-Component1-text3'>1870</p>
